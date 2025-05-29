f07a4f8015d5c3a30f794ae576c06d3803745d68
Serhii Bryk <brykseryi@gmail.com>
2025-05-29 16:21:23 +0300

    fix: notification mobile styles and main container padding

--- src/styles/diceSnackbarNotification.styles.ts
+++ src/styles/diceSnackbarNotification.styles.ts
@@ -5,17 +5,21 @@ export const StyledSnackbar = styled(Snackbar)(({ theme }) => ({
   width: '100%',
 
   '& .MuiPaper-root': {
-    borderRadius: theme.spacing(1),
-    width: '100%',
-    maxWidth: '600px',
-    marginLeft: 'auto',
     marginRight: 'auto',
+    marginLeft: 'auto',
+    width: '100%',
+    maxWidth: theme.spacing(75),
+    borderRadius: theme.spacing(1),
+
+    [theme.breakpoints.down('sm')]: {
+      maxWidth: '90%',
+    },
   },
 }));
 
 export const StyledAlert = styled(Alert)(({ theme }) => ({
-  borderRadius: theme.spacing(1),
   padding: theme.spacing(2, 3),
+  borderRadius: theme.spacing(1),
   color: '#fff',
 
   '&.MuiAlert-filledSuccess': {
